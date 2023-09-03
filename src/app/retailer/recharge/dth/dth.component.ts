f98c740e15e94337d314c673bddb08d7017d2f09
Amit Dubey <amitdubey@shyplite.com>
2023-09-03 17:11:10 +0530

    Display DTH

--- src/app/retailer/recharge/dth/dth.component.ts
+++ src/app/retailer/recharge/dth/dth.component.ts
@@ -8,13 +8,11 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-dth',
   templateUrl: './dth.component.html',
-  styleUrls: ['./dth.component.css']
+  styleUrls: ['./dth.component.css'],
 })
 export class DthComponent implements OnInit {
-
   boardList: any = [];
-  utilityModal: any = {
-  };
+  utilityModal: any = {};
   billerDetails: any = null;
   paymentSection = true;
   openModal = false;
@@ -24,14 +22,14 @@ export class DthComponent implements OnInit {
   toDt: any = new Date();
   billList: any;
   fetchpipeList: any;
-
+  displayDTH = false;
   constructor(
     public commonService: ApiService,
     public dataService: DataService,
     public route: ActivatedRoute,
     public router: Router,
-    public location: Location) {
-  }
+    public location: Location
+  ) {}
 
   ngOnInit(): void {
     this.fromDt.setDate(this.fromDt.getDate() - 29);
@@ -39,61 +37,79 @@ export class DthComponent implements OnInit {
     this.getRecentTransaction();
     this.getOperatorPipe();
   }
-
-  getoprators(docType:any,apiType:any): any {
-    if (this.commonService.userPram.userId !== undefined && this.commonService.userPram.userId !== '' && this.commonService.userPram.rmn !== undefined && this.commonService.userPram.rmn !== '') {
+  toggle(): any {
+    this.displayDTH = !this.displayDTH;
+  }
+  getoprators(docType: any, apiType: any): any {
+    if (
+      this.commonService.userPram.userId !== undefined &&
+      this.commonService.userPram.userId !== '' &&
+      this.commonService.userPram.rmn !== undefined &&
+      this.commonService.userPram.rmn !== ''
+    ) {
       this.commonService.isLoader = true;
-      this.commonService.getAuth('commonapi/get-oprators?docType='+docType +'&apiType='+apiType)
+      this.commonService
+        .getAuth(
+          'commonapi/get-oprators?docType=' + docType + '&apiType=' + apiType
+        )
         .subscribe(
           (res: any) => {
             this.commonService.isLoader = false;
             if (res.isSuccess) {
               this.boardList = res.respData;
-            }
-            else
-            {
-              this.boardList= [];
+            } else {
+              this.boardList = [];
             }
           },
           (err: any) => {
             console.log(err);
             this.commonService.isLoader = false;
             alert('something went wrong!');
-          });
+          }
+        );
     }
   }
   getOperatorPipe(): any {
     this.commonService.isLoader = true;
     if (this.commonService.userPram.userId && this.commonService.userPram.rmn) {
-      this.commonService.getAuth('recharge/get-api-service?isMaxAmt=false&ApiType=BBPS&Category=BILLFETCH&maxAmount=0')
+      this.commonService
+        .getAuth(
+          'recharge/get-api-service?isMaxAmt=false&ApiType=BBPS&Category=BILLFETCH&maxAmount=0'
+        )
         .subscribe(
           (res: any) => {
             this.commonService.isLoader = false;
             if (res.isSuccess && res.respData) {
-              this.fetchpipeList=res.respData;
-              if (res.respData.apiId === '08d93b98-21fc-49c9-8130-9ad5f53a3c9a') {
-                this.getoprators('GAS','icore');
-              } else if (res.respData.apiId === '8b53435a-faae-11eb-9029-f80dac58cdd1') {
-                this.getoprators('GAS','levin');
-              }
-              else
-              {
-                this.getoprators('GAS','mobik');
+              this.fetchpipeList = res.respData;
+              if (
+                res.respData.apiId === '08d93b98-21fc-49c9-8130-9ad5f53a3c9a'
+              ) {
+                this.getoprators('GAS', 'icore');
+              } else if (
+                res.respData.apiId === '8b53435a-faae-11eb-9029-f80dac58cdd1'
+              ) {
+                this.getoprators('GAS', 'levin');
+              } else {
+                this.getoprators('GAS', 'mobik');
               }
             }
           },
           (err: any) => {
             console.log(err);
             this.commonService.isLoader = false;
-          });
+          }
+        );
     }
   }
-
 
   getPaymentPipe(maxAmount: any): any {
     this.commonService.isLoader = true;
     if (this.commonService.userPram.userId && this.commonService.userPram.rmn) {
-      this.commonService.getAuth('recharge/get-api-service?isMaxAmt=true&ApiType=BBPS&Category=GAS&maxAmount=' + maxAmount)
+      this.commonService
+        .getAuth(
+          'recharge/get-api-service?isMaxAmt=true&ApiType=BBPS&Category=GAS&maxAmount=' +
+            maxAmount
+        )
         .subscribe(
           (res: any) => {
             console.log(res);
@@ -109,7 +125,8 @@ export class DthComponent implements OnInit {
             console.log(err);
             this.commonService.isLoader = false;
             alert('something went wrong!');
-          });
+          }
+        );
     }
   }
 
@@ -129,7 +146,7 @@ export class DthComponent implements OnInit {
 
   getBoard(board: any): any {
     let name = '';
-    console.log( this.boardList,board);
+    console.log(this.boardList, board);
     this.boardList.forEach((element: any) => {
       if (element.id === board) {
         name = element.name;
@@ -145,33 +162,42 @@ export class DthComponent implements OnInit {
       billNumber: this.utilityModal.caNumber,
       billperator: this.utilityModal.board,
       type: 'GAS',
-      docType: 'P1'
+      docType: 'P1',
     };
     this.commonService.isLoader = true;
-    this.commonService.postAES256(`recharge/get-fetch-bill`, JSON.stringify(passingData)).subscribe((res: any) => {
-      console.log(res);
-      this.commonService.isLoader = false;
-      if (res.isSuccess && res.respData && res.respData.isSuccess) {
-        this.commonService.displayToaster(res.mhOutcome, 'success');
-        this.paymentSection = false;
-        this.billerDetails = res.respData;
-        this.utilityModal.paymentAmount = res.respData.billamount;
-        this.getPaymentPipe(res.respData.billamount);
-      } else {
-        this.commonService.displayToaster(res.mhOutcome, 'error');
-      }
-    }, (err: any) => {
-      console.log(err);
-      this.commonService.isLoader = false;
-      this.commonService.displayToaster('something went wrong', 'error');
-    });
+    this.commonService
+      .postAES256(`recharge/get-fetch-bill`, JSON.stringify(passingData))
+      .subscribe(
+        (res: any) => {
+          console.log(res);
+          this.commonService.isLoader = false;
+          if (res.isSuccess && res.respData && res.respData.isSuccess) {
+            this.commonService.displayToaster(res.mhOutcome, 'success');
+            this.paymentSection = false;
+            this.billerDetails = res.respData;
+            this.utilityModal.paymentAmount = res.respData.billamount;
+            this.getPaymentPipe(res.respData.billamount);
+          } else {
+            this.commonService.displayToaster(res.mhOutcome, 'error');
+          }
+        },
+        (err: any) => {
+          console.log(err);
+          this.commonService.isLoader = false;
+          this.commonService.displayToaster('something went wrong', 'error');
+        }
+      );
   }
 
   payNow(): any {
     if (this.pipeList && this.pipeList.apiId) {
-      const savedPin = this.commonService.decryptUsingAES256(this.commonService.userPram.mPin);
+      const savedPin = this.commonService.decryptUsingAES256(
+        this.commonService.userPram.mPin
+      );
       const walletBalance = this.commonService.pramwallet.walleT_BALANCE;
-      const paymentAmount = this.activeTab ? this.utilityModal.paymentAmount : this.billerDetails?.billamount;
+      const paymentAmount = this.activeTab
+        ? this.utilityModal.paymentAmount
+        : this.billerDetails?.billamount;
       if (paymentAmount < walletBalance) {
         if (this.utilityModal.mPin === savedPin) {
           const passingData = {
@@ -184,44 +210,70 @@ export class DthComponent implements OnInit {
             paymentMode: 'BBPS',
             perTrnsAmount: paymentAmount,
             accNo: this.utilityModal.caNumber,
-            mobileNumber:this.commonService.userPram.rmn,
-            intent:this.commonService.userPram.companyName+'-'+this.commonService.userPram.rmn+' -'+this.commonService.userPram.userType+''+this.commonService.userPram.id,
+            mobileNumber: this.commonService.userPram.rmn,
+            intent:
+              this.commonService.userPram.companyName +
+              '-' +
+              this.commonService.userPram.rmn +
+              ' -' +
+              this.commonService.userPram.userType +
+              '' +
+              this.commonService.userPram.id,
             docType: 'P1',
             operators: this.getBoard(this.utilityModal.board),
             operatorId: this.utilityModal.board,
             opId: this.utilityModal.board,
-            narration: "GAS Bill Payment",
+            narration: 'GAS Bill Payment',
             BeneName: this.billerDetails?.records?.customerName,
-            refNumber: this.utilityModal.mobileNumber
+            refNumber: this.utilityModal.mobileNumber,
           };
           console.log(passingData);
           this.commonService.isLoader = true;
-          this.commonService.postAES256('recharge/bill-pay', JSON.stringify(passingData))
+          this.commonService
+            .postAES256('recharge/bill-pay', JSON.stringify(passingData))
             .subscribe(
               (res: any) => {
                 console.log(res);
                 this.commonService.isLoader = false;
-                Swal.fire({ icon: res.isSuccess ? 'success' : 'error', text: res.mhOutcome, confirmButtonText: 'OK' }).then(() => {
+                Swal.fire({
+                  icon: res.isSuccess ? 'success' : 'error',
+                  text: res.mhOutcome,
+                  confirmButtonText: 'OK',
+                }).then(() => {
                   if (res.isSuccess) {
-                    this.router.navigate(['/retailer/bill-payment/receipt'], { queryParams: { txnNo: res.txnNo } });
+                    this.router.navigate(['/retailer/bill-payment/receipt'], {
+                      queryParams: { txnNo: res.txnNo },
+                    });
                   }
                 });
               },
               (err: any) => {
                 console.log(err);
                 this.commonService.isLoader = false;
-              });
+              }
+            );
         } else {
-          Swal.fire({ icon: 'warning', text: 'Invalid mPin', confirmButtonText: 'OK' });
+          Swal.fire({
+            icon: 'warning',
+            text: 'Invalid mPin',
+            confirmButtonText: 'OK',
+          });
         }
       } else {
-        Swal.fire({ icon: 'warning', text: 'Insufficient wallet balance', confirmButtonText: 'OK' });
+        Swal.fire({
+          icon: 'warning',
+          text: 'Insufficient wallet balance',
+          confirmButtonText: 'OK',
+        });
       }
     } else {
-      Swal.fire({ icon: 'warning', text: 'Invalid payment amount', confirmButtonText: 'OK' });
+      Swal.fire({
+        icon: 'warning',
+        text: 'Invalid payment amount',
+        confirmButtonText: 'OK',
+      });
     }
   }
-
 
   toggleModal(): any {
     this.openModal = !this.openModal;
@@ -231,22 +283,43 @@ export class DthComponent implements OnInit {
     this.paymentSection = true;
   }
   getRecentTransaction(): void {
-    const fromDt = this.commonService.formatDate((this.fromDt ? this.fromDt : new Date()), 'yyyy-mm-dd');
-    const toDt = this.commonService.formatDate((this.toDt ? this.toDt : new Date()), 'yyyy-mm-dd');
-    if (this.commonService.userPram.userId && this.commonService.userPram.memberId) {
+    const fromDt = this.commonService.formatDate(
+      this.fromDt ? this.fromDt : new Date(),
+      'yyyy-mm-dd'
+    );
+    const toDt = this.commonService.formatDate(
+      this.toDt ? this.toDt : new Date(),
+      'yyyy-mm-dd'
+    );
+    if (
+      this.commonService.userPram.userId &&
+      this.commonService.userPram.memberId
+    ) {
       this.commonService.isLoader = true;
-      this.commonService.getAuth('retailerreport/get-recent-transaction?memberId=' + this.commonService.userPram.memberId + '&fromDt=' + fromDt + '&toDt=' + toDt + '&docType=GAS')
+      this.commonService
+        .getAuth(
+          'retailerreport/get-recent-transaction?memberId=' +
+            this.commonService.userPram.memberId +
+            '&fromDt=' +
+            fromDt +
+            '&toDt=' +
+            toDt +
+            '&docType=GAS'
+        )
         .subscribe(
           (res: any) => {
             this.commonService.isLoader = false;
             if (res.isSuccess) {
-              this.billList = JSON.parse(this.commonService.decryptUsingAES256(res.respData));
+              this.billList = JSON.parse(
+                this.commonService.decryptUsingAES256(res.respData)
+              );
               console.log(this.billList);
             }
           },
           (err: any) => {
             this.commonService.isLoader = false;
-          });
+          }
+        );
     }
   }
   changeText(myText: any): void {
@@ -255,7 +328,9 @@ export class DthComponent implements OnInit {
 
   printReceipt(data: any): any {
     if (data) {
-      this.router.navigate(['/retailer/bill-payment/receipt'], { queryParams: { txnNo: data } });
+      this.router.navigate(['/retailer/bill-payment/receipt'], {
+        queryParams: { txnNo: data },
+      });
     }
   }
 }
